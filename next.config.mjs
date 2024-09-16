4dc03d9f50bc533d739a14afa0e431e74842c8ef
Masaya Suzuki <draftcode@gmail.com>
2024-09-15 23:47:52 -0700

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -19,5 +21,9 @@ const nextConfig = {
     ],
   },
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
